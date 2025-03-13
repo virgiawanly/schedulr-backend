95f715bce180bcd2ad8bfcda33e69bed4b0214ff
Virgiawan Listiyandi <listiyandiv@gmail.com>
2025-03-13 15:13:59 +0700

    feat(proto): account and user proto

---
+++ protogen/go.mod
@@ -0,0 +1,17 @@
+module github.com/virgiawanly/schedulr-backend/protogen
+
+go 1.24.0
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
+)
+
+require (
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
+)
---
+++ scripts/proto_generate_go.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+WORKDIR="$SCRIPT_DIR/.."
+PROTODIR="$WORKDIR/proto"
+OUTDIR="$WORKDIR/protogen/go"
+GATEWAYOUTDIR="$WORKDIR/protogen/gateway/go"
+
+# Ensure output directory exists
+mkdir -p "$OUTDIR"
+mkdir -p "$GATEWAYOUTDIR"
+
+# Find and compile all .proto files recursively
+find "$PROTODIR" -name "*.proto" | while read -r entry; do
+  echo "Processing: $entry"
+  protoc --proto_path="$PROTODIR" \
+    --go_out="$OUTDIR" --go_opt=paths=source_relative \
+    --go-grpc_out="$OUTDIR" --go-grpc_opt=paths=source_relative \
+    --grpc-gateway_out "$GATEWAYOUTDIR" \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative \
+    --grpc-gateway_opt standalone=true \
+    --grpc-gateway_opt generate_unbound_methods=true \
+    "$entry"
+done
+
+echo "Proto files generated successfully in $OUTDIR"
